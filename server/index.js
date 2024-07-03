4338c573282997d4a5211177a155317d81ff4745
Arun Lohani <arunlohani007@gmail.com>
2024-07-03 19:29:49 +0530

    BUG FIX

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000; // Default port 8000 if PORT is not defined in .env
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -12,15 +12,16 @@ const io = new Server(httpServer, {
 });
 
 const PLAYERS_PER_GAME = 2;
+const playerQueue = [];
 
 class Player {
   constructor(id, socket) {
     this.id = id;
     this.socket = socket;
+    this.username = ""; // Initialize username property
+    this.opponent = null; // Initialize opponent property
   }
 }
-
-const playerQueue = [];
 
 function matchPlayers() {
   if (playerQueue.length < PLAYERS_PER_GAME) {
@@ -34,7 +35,7 @@ function matchPlayers() {
   match.players.forEach((player, index) => {
     const opponent = match.players.find((p, idx) => idx !== index);
     player.opponent = opponent.socket;
-    const shape = index == 0 ? "circle" : "cross";
+    const shape = index === 0 ? "circle" : "cross";
     player.socket.emit("match", {
       opponentName: opponent.username,
       shape: shape,
@@ -43,10 +44,9 @@ function matchPlayers() {
 }
 
 io.on("connection", (socket) => {
-  console.log("user has connected");
+  console.log("User has connected");
 
   const player = new Player(socket.id, socket);
-
   playerQueue.push(player);
 
   socket.on("request_to_play", (data) => {
@@ -57,8 +57,7 @@ io.on("connection", (socket) => {
     }
 
     socket.on("playAgain", () => {
-      delete player.opponent
-      
+      delete player.opponent;
       playerQueue.push(player);
 
       if (playerQueue.length >= PLAYERS_PER_GAME) {
@@ -67,24 +66,19 @@ io.on("connection", (socket) => {
     });
 
     socket.on("PlayerMove", (data) => {
-      //  console.log(data);
-      player.opponent.emit("updateGameState", {
-        ...data
-      });
+      if (player.opponent) {
+        player.opponent.emit("updateGameState", { ...data });
+      }
     });
 
-    player.opponent?.on("PlayerMove",(data)=>{
-
-      // console.log(data)
-      socket.emit("updateGameState", {
-        ...data,
-      });
-    })
+    player.opponent?.on("PlayerMove", (data) => {
+      socket.emit("updateGameState", { ...data });
+    });
 
     socket.on("changeTurn", (data) => {
-      player.opponent.emit("changeTurn", {
-        turn: data.turn,
-      });
+      if (player.opponent) {
+        player.opponent.emit("changeTurn", { turn: data.turn });
+      }
     });
 
     socket.on("disconnect", () => {
@@ -92,14 +86,10 @@ io.on("connection", (socket) => {
 
       const index = playerQueue.findIndex((p) => p.id === player.id);
 
-      console.log(index);
-
       if (index !== -1) {
         playerQueue.splice(index, 1);
-      } else {
-        // console.log(player.opponent);
+      } else if (player.opponent) {
         player.opponent.emit("opponentWithdraw", {});
-
         delete player.opponent;
       }
     });
